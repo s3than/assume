e9f4fff77bcf52fda6893579becccaadc1d64c25
Tim Colbert <admin@tcolbert.net>
2017-11-22 10:50:33 +1000

    Create assume role application

---
+++ README.md
@@ -0,0 +1,88 @@
+# AWS Assume
+
+Command line application to assume a role using a config.ini and config.creds file, the config.ini utilises the same file format as [aws-extend-switch-roles](https://github.com/tilfin/aws-extend-switch-roles). This then merges a config.creds file to authorise assume roles
+
+## Getting Started
+
+Create a config file that can be used in the format from aws-extend-switch-roles, create a config.creds file with the following values.
+
+    # Master account
+    [profile master]
+    aws_access_key_id = AWS_ACCESS_KEY_ID
+    aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+    secret = TOTP_SECRET
+    region = ap-southeast-2
+
+
+[![Codefresh build status]( https://g.codefresh.io/api/badges/build?repoOwner=s3than&repoName=assume&branch=master&pipelineName=assume&accountName=s3than&key=eyJhbGciOiJIUzI1NiJ9.NTk2ODRiMGZkODc0NjAwMDAxZjhhNmFj.lSAMiveZ-q1di_6aNrTtS4jnpm7I3P9WS__6D8R7xuw&type=cf-1)]( https://g.codefresh.io/repositories/s3than/assume/builds?filter=trigger:build;branch:master;service:5a12afe8b7f5280001d1d04f~assume)
+
+<!-- ### Prerequisites
+
+NA
+
+### Installing
+
+A step by step series of examples that tell you have to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system -->
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/s3than/assume/tags).
+
+## Authors
+
+* **Tim Colbert ** - *Initial work* - [S3than](https://github.com/s3than)
+
+See also the list of [contributors](https://github.com/s3than/assume/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone who's code was used
+* Inspiration
+* etc
+
---
+++ main.go
@@ -0,0 +1,339 @@
+// Copyright © 2017 Tim Colbert admin@tcolbert.net
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/user"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/go-ini/ini"
+	"github.com/pquerna/otp/totp"
+)
+
+// AwsAccount struct to hold base account details
+type AwsAccount struct {
+	region          string
+	accessKeyID     string
+	secretAccessKey string
+}
+
+// MfaToken token configuration
+type MfaToken struct {
+	serialNumber string
+	tokenCode    string
+}
+
+// AssumeCredentials Credential details
+type AssumeCredentials struct {
+	profile  string
+	secret   string
+	account  AwsAccount
+	roleArn  string
+	session  client.ConfigProvider
+	mfaToken MfaToken
+}
+
+var configPath string
+var configCredsPath string
+
+var defaultConfig = "/.config/assume/config.ini"
+var defaultCreds = "/.config/assume/config.creds"
+
+func main() {
+
+	config := flag.String(
+		"config",
+		"",
+		"config file (default is $HOME/.config/assume/config.ini)",
+	)
+	configCreds := flag.String(
+		"credentials",
+		"",
+		"credentials file (default is $HOME/.config/assume/config.creds)",
+	) // for naming bucket
+	accountRef := flag.String(
+		"account",
+		"default",
+		"AWS account reference",
+	)
+	flag.Parse()
+
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	if *config == "" {
+		configPath = usr.HomeDir + defaultConfig
+	} else {
+		configPath = *config
+	}
+
+	if *configCreds == "" {
+		configCredsPath = usr.HomeDir + defaultCreds
+	} else {
+		configCredsPath = *config
+	}
+
+	os.OpenFile(configPath, os.O_CREATE, 0666)
+	configFile, err := ini.Load(configPath)
+
+	os.OpenFile(configCredsPath, os.O_CREATE, 0666)
+	configFile.Append(configCredsPath)
+
+	creds, err := getCredentials(configFile, *accountRef, AssumeCredentials{})
+
+	if creds.profile != "" {
+		creds, err = getCredentials(configFile, creds.profile, creds)
+	}
+
+	if err != nil {
+		fmt.Printf("%+v", err)
+		os.Exit(1)
+	}
+
+	if isConfigured(creds.account) {
+		awsCreds := &sts.Credentials{}
+		awsCreds, err = assumeRole(creds)
+
+		if err != nil {
+			fmt.Printf("%+v", err)
+			os.Exit(1)
+		}
+
+		if awsCreds != nil {
+			account := creds.account
+
+			err = writeFile(awsCreds, usr, account.region)
+
+			if err != nil {
+				fmt.Printf("%+v", err)
+				os.Exit(1)
+			}
+
+			os.Exit(0)
+		}
+		os.Exit(1)
+	}
+}
+
+func writeFile(awsCreds *sts.Credentials, usr *user.User, region string) error {
+	credFilePath := usr.HomeDir + "/.aws/credentials"
+
+	os.OpenFile(credFilePath, os.O_CREATE, 0666)
+	credFile, err := ini.Load(credFilePath)
+	if err != nil {
+		return err
+	}
+	credSect, err := credFile.NewSection("default")
+	if err != nil {
+		return err
+	}
+	credSect.NewKey("aws_access_key_id", *awsCreds.AccessKeyId)
+	credSect.NewKey("aws_secret_access_key", *awsCreds.SecretAccessKey)
+	credSect.NewKey("aws_session_token", *awsCreds.SessionToken)
+	// Legacy support for boto apps
+	credSect.NewKey("aws_security_token", *awsCreds.SessionToken)
+	credSect.NewKey("region", region)
+	credSect.NewKey("output", "json")
+
+	credFile.SaveTo(credFilePath)
+
+	// Config file details
+	configFilePath := usr.HomeDir + "/.aws/config"
+	os.OpenFile(configFilePath, os.O_CREATE, 0666)
+	configFile, err := ini.Load(configFilePath)
+	if err != nil {
+		return err
+	}
+	configSect, err := configFile.NewSection("default")
+	if err != nil {
+		return err
+	}
+	configSect.NewKey("region", region)
+	configSect.NewKey("output", "json")
+	configFile.SaveTo(configFilePath)
+
+	return err
+}
+
+func assumeRole(creds AssumeCredentials) (*sts.Credentials, error) {
+
+	mfaToken := creds.mfaToken
+
+	var assumeRoleOutput *sts.AssumeRoleOutput        // nil
+	var sessionTokenOutput *sts.GetSessionTokenOutput // nil
+	var credentials *sts.Credentials
+	var err error
+
+	switch {
+	case mfaToken.serialNumber != "" && creds.profile != "":
+		assumeRoleOutput, err = sts.New(creds.session).AssumeRole(&sts.AssumeRoleInput{
+			RoleArn:         aws.String(creds.roleArn),
+			RoleSessionName: aws.String(randStringBytesMaskImprSrc(6)),
+			SerialNumber:    aws.String(mfaToken.serialNumber),
+			TokenCode:       aws.String(mfaToken.tokenCode),
+		})
+		return assumeRoleOutput.Credentials, err
+	case mfaToken.serialNumber != "" && creds.profile == "":
+		sessionTokenOutput, err = sts.New(creds.session).GetSessionToken(&sts.GetSessionTokenInput{
+			SerialNumber: aws.String(mfaToken.serialNumber),
+			TokenCode:    aws.String(mfaToken.tokenCode),
+		})
+		return sessionTokenOutput.Credentials, err
+	case mfaToken.serialNumber == "" && creds.profile != "":
+		assumeRoleOutput, err = sts.New(creds.session).AssumeRole(&sts.AssumeRoleInput{
+			RoleArn:         aws.String(creds.roleArn),
+			RoleSessionName: aws.String(randStringBytesMaskImprSrc(6)),
+		})
+		return assumeRoleOutput.Credentials, err
+	case mfaToken.serialNumber == "" && creds.profile == "":
+		sessionTokenOutput, err = sts.New(creds.session).GetSessionToken(&sts.GetSessionTokenInput{})
+		return sessionTokenOutput.Credentials, err
+	}
+
+	return credentials, errors.New("No session set")
+}
+
+func getCredentials(config *ini.File, account string, creds AssumeCredentials) (AssumeCredentials, error) {
+	section, err := config.GetSection(account)
+
+	if section == nil {
+		section, err = config.GetSection("profile " + account)
+	}
+
+	if section == nil {
+		return creds, errors.New("No AWS Config located")
+	}
+
+	profile, err := section.GetKey("source_profile")
+	if profile != nil {
+		creds.profile = profile.String()
+	}
+
+	secret, err := section.GetKey("secret")
+	if secret != nil {
+		creds.secret = secret.String()
+	}
+
+	awsAccount := AwsAccount{}
+
+	region, err := section.GetKey("region")
+	if region != nil {
+		awsAccount.region = region.String()
+	} else if region == nil {
+		awsAccount.region = "ap-southeast-2"
+	}
+
+	awsAccessKeyID, err := section.GetKey("aws_access_key_id")
+	if awsAccessKeyID != nil {
+		awsAccount.accessKeyID = awsAccessKeyID.String()
+	}
+
+	awsSecretAccessKey, err := section.GetKey("aws_secret_access_key")
+	if awsSecretAccessKey != nil {
+		awsAccount.secretAccessKey = awsSecretAccessKey.String()
+	}
+
+	creds.account = awsAccount
+
+	roleArn, err := section.GetKey("role_arn")
+
+	if roleArn != nil {
+		creds.roleArn = roleArn.String()
+	}
+
+	// Set AWS Session
+	awsSession := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String(awsAccount.region),
+			Credentials: credentials.NewCredentials(&credentials.StaticProvider{Value: credentials.Value{
+				AccessKeyID:     awsAccount.accessKeyID,
+				SecretAccessKey: awsAccount.secretAccessKey,
+			}}),
+		},
+		AssumeRoleTokenProvider: stscreds.StdinTokenProvider,
+		SharedConfigState:       session.SharedConfigEnable,
+	}))
+
+	creds.session = awsSession
+
+	devices, err := iam.New(creds.session).ListMFADevices(&iam.ListMFADevicesInput{})
+
+	if len(devices.MFADevices) != 0 {
+		token := ""
+		awsToken := MfaToken{}
+		if creds.secret == "" {
+			token, err = stscreds.StdinTokenProvider()
+		} else {
+			token, err = totp.GenerateCode(creds.secret, time.Now())
+		}
+		awsToken.tokenCode = token
+		awsToken.serialNumber = *devices.MFADevices[0].SerialNumber
+		creds.mfaToken = awsToken
+	}
+
+	return creds, err
+}
+
+// isConfigured Return boolean if account details are set
+func isConfigured(a AwsAccount) bool {
+
+	if a.accessKeyID == "" || a.secretAccessKey == "" {
+		return false
+	}
+
+	return true
+}
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+// randStringBytesMaskImprSrc generate random string
+func randStringBytesMaskImprSrc(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
